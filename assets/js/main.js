e8a920994cfd4e44b64daab287c998f4d654a71c
sami <ahmedhossain.buet@gmail.com>
2025-07-08 09:15:31 +0600

    added favicons, solved relative links issue, fixed theme switcher

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,3 @@
-// Theme Toggle Button
-const toggle = document.getElementById('theme-toggle');
-const currentTheme = localStorage.getItem('theme');
-if (currentTheme === 'dark') {
-  toggle.checked = true;
-}
-
-toggle.addEventListener('change', function () {
-  if (this.checked) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme', 'dark');
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light');
-    localStorage.setItem('theme', 'light');
-  }
-});
-// ============================ END of Theme Toggle ===============================
-
-
 // Image Lazy Load
 // https://github.com/aFarkas/lazysizes/tree/gh-pages/plugins/native-loading
 import lazySizes from 'lazysizes';
